c753277e236023dfa1c4f5902bf6b11aac221fe4
David Nunes <98975085+davidygn17@users.noreply.github.com>
2022-07-01 22:26:48 +0100

    Debug Cetada

---
+++ .gitgnore.py
@@ -0,0 +1,18 @@
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.robeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
+
+# postgres files local development
+.pgdata
--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6mfgo$96gm@o5z6n-uu-)hvubw*g9+j4n5wjewk++($+cku&!z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
