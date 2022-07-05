726399e2ac9938cdb74313ec8f4110ed8ecfe5bb
David Nunes <98975085+davidygn17@users.noreply.github.com>
2022-07-05 23:40:32 +0100

    corrigindo hora

--- pypro/settings.py
+++ pypro/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-pt'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
