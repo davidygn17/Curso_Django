f89f31bef0dde5b720ed93c46656bea1cc39d1de
David Nunes <98975085+davidygn17@users.noreply.github.com>
2022-07-05 23:36:10 +0100

    heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-pt'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'Europa/Lisboa'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
